3638e38a121d90f36a7028ef4d8dd3a46a600deb
Travis Ueki <travueki@gmail.com>
2020-06-22 11:46:33 -0500

    add typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Typescript Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Typescript Starter
         </h1>
 
         <p className="description">
