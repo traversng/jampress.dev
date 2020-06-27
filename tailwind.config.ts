9f8eb2801e3bf9aa14562b026bf22aae472ec1af
Travis Ueki <travueki@gmail.com>
2020-06-27 12:59:26 -0500

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,5 +3,11 @@ import toTailwind from '@theme-ui/tailwind'
 import theme from './styles/theme' // Path to Theme UI config
 module.exports = {
   ...toTailwind(theme),
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}']
+  purge: {
+    enabled: true,
+    content: [
+      './components/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+    ],
+  },
 }
