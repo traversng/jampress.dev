1b5bcd4de87f43a04102f3e8ed245546470b00ed
Travis Ueki <travueki@gmail.com>
2020-06-27 12:40:04 -0500

    purge tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 // tailwind.config.js
 import toTailwind from '@theme-ui/tailwind'
 import theme from './styles/theme' // Path to Theme UI config
-module.exports = toTailwind(theme)
+module.exports = {
+  ...toTailwind(theme),
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}']
+}
