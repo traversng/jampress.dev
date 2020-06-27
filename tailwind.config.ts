22e5c97028d04d12e1395bf61197ff5005790a48
Travis Ueki <travueki@gmail.com>
2020-06-27 17:12:12 -0500

    fix purge again??

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,4 @@
 // tailwind.config.js
 import toTailwind from '@theme-ui/tailwind'
 import theme from './styles/theme' // Path to Theme UI config
-module.exports = {
-  purge: {
-    enabled: true,
-    content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  },
-  theme: {},
-  variants: {},
-  plugins: [],
-  ...toTailwind(theme),
-}
+module.exports = toTailwind({...theme, purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}']})
